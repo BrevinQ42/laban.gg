6d4d25afd904a5bcd0608a59495803accd3c9f63
Migs Atienza <160925292+SpokBlok@users.noreply.github.com>
2024-02-25 16:11:18 +0800

    Created Account Model

--- labanGG/register/models.py
+++ labanGG/register/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    username = models.CharField(max_length=32)
+    email = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=32)
+    isOrganizer = models.BooleanField(default=False)
