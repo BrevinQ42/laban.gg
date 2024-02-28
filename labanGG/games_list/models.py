7b8f0220bed0a527ee349c3134d1555a0f29864f
Jo <joei.yucoco@obf.ateneo.edu>
2024-02-28 19:06:56 +0800

    Added models
    - also added some data in admin

--- labanGG/games_list/models.py
+++ labanGG/games_list/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to = 'images', null = True, blank = True)
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
 # Create your models here.
