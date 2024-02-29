9be7ec5766281db27a795a816251ddf957586212
Stefan Gomez <enrique.gomez@obf.ateneo.edu>
2024-02-29 23:11:53 +0800

    Added the templates folder and created the base_organizer.html file in it
    Base_organizer file contains the header and side panels for tournament organizers POV.

--- labanGG/labanGG/settings.py
+++ labanGG/labanGG/settings.py
@@ -59,6 +59,7 @@ ROOT_URLCONF = 'labanGG.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
