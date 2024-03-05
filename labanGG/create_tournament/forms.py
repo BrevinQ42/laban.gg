bd75396c6e242439a3a2496df4a2333f7fa00c7e
Stefan Gomez <enrique.gomez@obf.ateneo.edu>
2024-03-05 16:26:41 +0800

    Made it such that when the user inputs information and publishes the tournament, the tournament details that the user filled up are saved to the database.

--- labanGG/create_tournament/forms.py
+++ labanGG/create_tournament/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from .models import Tournament
 
 class TournamentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['application_link'].required = False
+
     class Meta:
         model = Tournament
         fields = ['name', 'game', 'tier', 'location', 'format', 'application_link', 'schedule', 'prize_pool', 'more_details', 'image']
--- labanGG/create_tournament/views.py
+++ labanGG/create_tournament/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
 from .forms import TournamentForm
+from .models import Tournament
 
 def create_tournament(request):
     if request.method == 'POST':
         form = TournamentForm(request.POST, request.FILES)
         if form.is_valid():
+            # Check if image is provided
+            if 'image' not in request.FILES:
+                # If no image provided, assign a default image
+                form.instance.image = "https://i.gyazo.com/cbb5ad244a3e28c3dd89906f62f179fe.png"  
+
             form.save()
             return redirect('tournament_list')  # Redirect to a page showing all tournaments
     else:
