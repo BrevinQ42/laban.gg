2f36644fd610c2798da0c2274a123981b31692c2
Nate Brevin Que <nate.que@obf.ateneo.edu>
2024-03-12 13:03:45 +0800

    Created the header of the page (the title of the page, and the search bar which already accepts user input and this input is accessible in views.py

--- labanGG/player_applications_list/views.py
+++ labanGG/player_applications_list/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
-	return render(request, 'player_applications_list/index.html', {})
+	context = {}
+	keyword = ""
+
+	if request.POST.get('searchPlayer'):
+		keyword = request.POST.get('usedKeyword')
+
+	return render(request, 'player_applications_list/index.html', context)
