e0bec439a7ed3e1825d0f61a78b695792e6387aa
Nate Brevin Que <nate.que@obf.ateneo.edu>
2024-03-17 20:39:26 +0800

    Added search functionality based on the usernames of the applicants (which are temporarily all accounts)

--- labanGG/player_applications_list/views.py
+++ labanGG/player_applications_list/views.py
@@ -4,6 +4,7 @@ from register.models import Account
 
 
 def index(request):
+
 	context = {}
 	context['default_player_icon'] = "/account_profile_images/default.png"
 
@@ -11,6 +12,10 @@ def index(request):
 
 	if request.POST.get('searchPlayer'):
 		keyword = request.POST.get('usedKeyword')
-	context['applicants'] = Account.objects.all()
+		applicants = Account.objects.filter(username__contains=keyword)
+	else:
+		applicants = Account.objects.all()
+
+	context['applicants'] = applicants
 
 	return render(request, 'player_applications_list/index.html', context)
