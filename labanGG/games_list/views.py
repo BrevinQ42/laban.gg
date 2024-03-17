c05c1c6f593206f0ebf810cdec0bc21f202a15da
Jo <joei.yucoco@obf.ateneo.edu>
2024-03-17 14:39:36 +0800

    added search functionality

--- labanGG/games_list/views.py
+++ labanGG/games_list/views.py
@@ -9,9 +9,18 @@ class GamesListView(ListView):
     model = Game
     template_name = 'games_list/index.html'
 
+
 class GamesDetailView(DetailView):
     model = Game
     template_name = 'games_list/tournaments_per_game.html'
+
+class GamesSearchView(ListView):
+    model = Game
+    template_name = 'games_list/index.html'
+    
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Game.objects.filter(title__icontains=query).order_by('title')
         
 
 # Create your views here.
