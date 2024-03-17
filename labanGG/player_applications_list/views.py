e9872c5f19382b926f6a520b39928305707c2ce9
Nate Brevin Que <nate.que@obf.ateneo.edu>
2024-03-17 20:36:21 +0800

    Created the whole front-end of the page

--- labanGG/player_applications_list/views.py
+++ labanGG/player_applications_list/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 
+from register.models import Account
+
 
 def index(request):
 	context = {}
+	context['default_player_icon'] = "/account_profile_images/default.png"
+
 	keyword = ""
 
 	if request.POST.get('searchPlayer'):
 		keyword = request.POST.get('usedKeyword')
+	context['applicants'] = Account.objects.all()
 
 	return render(request, 'player_applications_list/index.html', context)
