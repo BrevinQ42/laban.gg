9e996e7eaa4d14765295ad583c0b7cbbc35bb1c7
Stefan Gomez <enrique.gomez@obf.ateneo.edu>
2024-03-24 22:16:39 +0800

    Added the edit tournament feature of this app.

--- labanGG/my_tournaments_organizer/views.py
+++ labanGG/my_tournaments_organizer/views.py
@@ -1,7 +1,7 @@
-# views.py
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from register.models import Account  
 from create_tournament.models import Tournament
+from create_tournament.forms import TournamentForm  
 
 def my_tournaments_organizer(request):
     user = request.user
@@ -12,3 +12,11 @@ def my_tournaments_organizer(request):
     else:
         # Handle the case where the user is not a Tournament Organizer
         return redirect('/games/')
+
+def edit_tournament(request, tournament_id):
+    tournament = get_object_or_404(Tournament, id=tournament_id)
+    form = TournamentForm(request.POST or None, instance=tournament)  
+    if form.is_valid():
+        form.save()
+        return redirect('/my_tournaments_organizer/')  # Redirect to my_tournaments_organizer page after saving
+    return render(request, 'edit_tournament.html', {'form': form, 'tournament': tournament})
