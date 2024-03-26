b3f8d3d32dbf5d1e8dabd799866ba2a2bd4e7422
Nate Brevin Que <nate.que@obf.ateneo.edu>
2024-03-26 12:33:11 +0800

    Configured it such that it will be known in views.py which application is involved when a check or X button is clicked on

--- labanGG/player_applications_list/views.py
+++ labanGG/player_applications_list/views.py
@@ -10,13 +10,25 @@ def index(request, id):
 	context = {}
 	context['default_player_icon'] = "/account_profile_images/default.png"
 
-	keyword = ""
+	keyword = ""	
+	applicants = Account.objects.filter(isOrganizer=False)
 
-	if request.POST.get('searchPlayer'):
-		keyword = request.POST.get('usedKeyword')
-		applicants = Account.objects.filter(username__contains=keyword)
-	else:
-		applicants = Account.objects.all()
+	if request.method == 'POST':
+		if request.POST.get('searchPlayer'):
+			keyword = request.POST.get('usedKeyword')
+			applicants = applicants.filter(username__contains=keyword)
+		else:
+			for applicant in applicants:
+				if request.POST.get('acceptApp' + str(applicant.id)):
+					# set application status to accepted
+					
+					context['message'] = applicant.username + "'s application was accepted."
+					break
+				elif request.POST.get('rejectApp' + str(applicant.id)):
+					# set application status to rejected
+
+					context['message'] = applicant.username + "'s application was rejected."
+					break
 
 	context['applicants'] = applicants
 
