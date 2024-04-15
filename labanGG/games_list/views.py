fd04dd925af5a0ed14e9efca58a64557baedf0a6
Stefan Gomez <enrique.gomez@obf.ateneo.edu>
2024-04-15 20:52:05 +0800

    Made the games-list (home page) extend the appropriate base html file depending on the logged in user

--- labanGG/games_list/views.py
+++ labanGG/games_list/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views import View
-from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Game
 
 class GamesListView(ListView):
     model = Game
     template_name = 'games_list/index.html'
 
+    def get_context_data(self, **kwargs): #Add this to determine which base_account.html to display
+        context = super().get_context_data(**kwargs)
+        context['base_template'] = 'base_organizer.html' if self.request.user.isOrganizer else 'base_attendee.html'
+        return context
 
 class GamesDetailView(DetailView):
     model = Game
     template_name = 'games_list/tournaments_per_game.html'
+
+    def get_context_data(self, **kwargs): #Add this to determine which base_account.html to display
+        context = super().get_context_data(**kwargs)
+        context['base_template'] = 'base_organizer.html' if self.request.user.isOrganizer else 'base_attendee.html'
+        return context
 
 class GamesSearchView(ListView):
     model = Game
@@ -21,6 +28,3 @@ class GamesSearchView(ListView):
     def get_queryset(self):
         query = self.request.GET.get('q')
         return Game.objects.filter(title__icontains=query).order_by('title')
-        
-
-# Create your views here.
