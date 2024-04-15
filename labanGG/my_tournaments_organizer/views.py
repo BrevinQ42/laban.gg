548efa08e0516ef892f8f44f652bc611201e27f2
Stefan Gomez <enrique.gomez@obf.ateneo.edu>
2024-04-15 19:21:49 +0800

    Added the search feature into the my_tournaments_organizer page.

--- labanGG/my_tournaments_organizer/views.py
+++ labanGG/my_tournaments_organizer/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render, redirect, get_object_or_404
 from register.models import Account  
 from create_tournament.models import Tournament
 from create_tournament.forms import TournamentForm  
+from django.db.models import Q  # Import Q for complex queries
 
 def my_tournaments_organizer(request):
     user = request.user
-    if user.isOrganizer == True:
-        organizerName = user.username
-        tournaments = Tournament.objects.filter(tournament_organizer=organizerName)
+    if user.isOrganizer:
+        organizer_name = user.username
+        query = request.GET.get('q')  # Get the search query from the URL parameters
+        tournaments = Tournament.objects.filter(tournament_organizer=organizer_name)
+
+        if query:
+            tournaments = tournaments.filter(Q(name__icontains=query))  # Filter tournaments by name
+
         return render(request, 'my_tournaments_organizer.html', {'tournaments': tournaments})
     else:
         # Handle the case where the user is not a Tournament Organizer
