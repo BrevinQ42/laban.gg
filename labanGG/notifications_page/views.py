168956309c7ad627f39004f7b3139bf3794cfa4b
Stefan Gomez <enrique.gomez@obf.ateneo.edu>
2024-04-18 23:43:20 +0800

    Added CSS to fit the formatting of the website

--- labanGG/notifications_page/views.py
+++ labanGG/notifications_page/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
 from .models import Notifications
 
+def get_base_template(request):
+    base_template = 'base_organizer.html' if request.user.isOrganizer else 'base_attendee.html'
+    return base_template
+
 def notifications_page(request):
     # Retrieve or create the notification settings for the current user
     notifications_page, created = Notifications.objects.get_or_create(account=request.user)
 
     if request.method == 'POST':
-        # Process the form submission
         notifications_page.notifications1 = request.POST.get('notifications1') == 'on'
         notifications_page.notifications2 = request.POST.get('notifications2') == 'on'
         notifications_page.notifications3 = request.POST.get('notifications3') == 'on'
         notifications_page.save()
-        # You can add a success message here if needed
+
+    base_template = get_base_template(request)
 
     context = {
         'notifications_page': notifications_page,
+        'base_template': base_template,
     }
     return render(request, 'notifications_page.html', context)
