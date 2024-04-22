11912996ffa5116d9b33bf9d9496024d24c82131
Stefan Gomez <enrique.gomez@obf.ateneo.edu>
2024-04-23 01:08:08 +0800

    Added search and made it only display tourneys where the attendee got accepted.

--- labanGG/my_tournaments_attendee/views.py
+++ labanGG/my_tournaments_attendee/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from join_tournament.models import TournamentPlayer
 from create_tournament.models import Tournament
+from django.db.models import Q  
 
 def my_tournaments_attendee(request):
     user = request.user
-    user_tournaments = TournamentPlayer.objects.filter(account=user)
+    user_tournaments = TournamentPlayer.objects.filter(account=user, application_status='Accepted')
+    query = request.GET.get('q')  
 
     # Fetch all tournaments
-    all_tournaments = Tournament.objects.all()
-
-    # Filter tournaments that the user has joined
     tournaments = [tournament.tournament for tournament in user_tournaments]
+
+    if query:
+        tournaments = [tournament for tournament in tournaments if query.lower() in tournament.name.lower()]
 
     context = {
         'tournaments': tournaments,
