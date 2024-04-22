8d2ea6d64825e98b5fc9d507d5059aca0ecd5fca
Stefan Gomez <enrique.gomez@obf.ateneo.edu>
2024-04-22 23:51:52 +0800

    Updated the models and html checkboxes to only have 1 notification

--- labanGG/notifications_page/models.py
+++ labanGG/notifications_page/models.py
@@ -4,4 +4,3 @@ from register.models import Account
 class Notifications(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE) # ForeignKey reference to account in register
     notifications1 = models.BooleanField(default=False) # Notification when a tournament joined is started. 
-    notifications2 = models.BooleanField(default=False)  
